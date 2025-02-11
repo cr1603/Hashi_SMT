c206719b57498d6eac0a4877d997449c2941fd72
Charlotte Ren <charlotte.ren@outlook.com>
2025-02-11 10:26:45 +0100

    bridges drawn not complete

--- output_solution.py
+++ output_solution.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def output_solution(width, height, island_info, bridge_list, bridge_value):
+def output_solution(width, height, island_info, bridge_list_unsorted, bridge_value):
     output = [["." for _ in range(width)] for _ in range(height)]
 
     x_coord = [x for x, y, v in island_info]
     y_coord = [y for x, y, v in island_info]
     value   = [v for x, y, v in island_info]
+
+    bridge_list = sorted(bridge_list_unsorted, key = lambda x: (x[0], x[1]))
+    print(bridge_list)
 
     if bridge_value == 0:
         island1 = [i1 for i1, i2, bv in bridge_list]
@@ -15,26 +18,59 @@ def output_solution(width, height, island_info, bridge_list, bridge_value):
         island1 = [i1 for i1, i2 in bridge_list]
         island2 = [i2 for i1, i2 in bridge_list]
 
+
     for i in range(len(x_coord)):
         output[x_coord[i]-1][y_coord[i]-1] = value[i]
 
-    # for i in range(len(bridge_list)):
-    #     if (x_coord[island1[i]-1]) == x_coord[island2[i]-1]:
-    #         j = x_coord[island1[i]-1]
-    #         while j < (x_coord[island2[i]-1]-1):
-    #             if bridge_value == 0:
-    #                 output[i][j] = "-"
-    #             else:
-    #                 output[i][j] = "-"
+    count = 0
 
     for i in range(height):
         for j in range(width):
             if output[i][j] == ".":
                 for a in range(len(island1)):
-                    if (x_coord[island1[a]-1]-1 == i) and (x_coord[island1[a]-1] == x_coord[island2[a]-1]):
-                        while j<y_coord[island2[a]-1]-1:
-                            output[i][j] = "-"
+                    if (x_coord[island1[a]-1]-1 == i) and (x_coord[island1[a]-1] == x_coord[island2[a]-1]): #horizontal bridges
+                        while j>y_coord[island1[a]-1]-1 and j<y_coord[island2[a]-1]-1:
+                            print(a)
+                            print(j)
+                            print(y_coord[island2[a]-1]-1)
+                            if bridge_value == 0:
+                                if bridge_values[a] == 1:
+                                    output[i][j] = "-"
+                                    print(np.matrix(output))
+                                elif bridge_values[a] == 2:
+                                    output[i][j] = "="
+                                    print(np.matrix(output))
+                            else:
+                                if bridge_value == 1:
+                                    output[i][j] = "-"
+                                    print(np.matrix(output))
+                                elif bridge_value == 2:
+                                    output[i][j] = "="
+                                    print(np.matrix(output))
                             j += 1
+                    elif (y_coord[island1[a]-1]-1 == j) and (y_coord[island1[a]-1] == y_coord[island2[a]-1]): #vertical bridges
+                        while i>x_coord[island1[a]-1]-1 and i<x_coord[island2[a]-1]-1:
+                            count = 0
+                            print(a)
+                            print(i)
+                            print(x_coord[island2[a]-1]-1)
+                            if bridge_value == 0:
+                                if bridge_values[a] == 1:
+                                    output[i][j] = "|"
+                                    print(np.matrix(output))
+                                elif bridge_values[a] == 2:
+                                    output[i][j] = "$"
+                                    print(np.matrix(output))
+                            else:
+                                if bridge_value == 1:
+                                    output[i][j] = "|"
+                                    print(np.matrix(output))
+                                elif bridge_value == 2:
+                                    output[i][j] = "$"
+                                    print(np.matrix(output))
+                            i += 1
+                            count += 1
+                        i -= count
     
     print("solution:")
     print(np.matrix(output))
