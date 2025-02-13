e3cd2840aa62dec54617c6d1fde03f58c87b3b43
Charlotte Ren <charlotte.ren@outlook.com>
2025-02-13 12:11:12 +0100

    changed number of islands in a generated game to be around 30%

--- generate_puzzle.py
+++ generate_puzzle.py
@@ -3,13 +3,17 @@ import random
 def generate_puzzle(height, width):
     puzzle = open("generated_puzzle.txt", "w")
 
-    cell_options = [".", "1", "2", "3", "4", "5", "6", "7", "8"]
+    cell_options = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
     puzzle.write(f"{height} {width}\n")
 
     for i in range(height):
         for j in range(width):
-            puzzle.write(random.choice(cell_options))
+            threshold = random.randrange(1, 10)
+            if threshold>3:
+                puzzle.write(".")
+            else:
+                puzzle.write(random.choice(cell_options))
         puzzle.write("\n")
 
 
